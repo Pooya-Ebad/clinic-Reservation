313141f7bb38c45b394cf21887bf7f3145fc6dc4
Pooya-Ebad <pooya7009@gmail.com>
2024-12-19 01:26:27 +0330

    bug fix

--- src/module/doctors/doctors.service.ts
+++ src/module/doctors/doctors.service.ts
@@ -23,8 +23,19 @@ export class DoctorsService {
     const { phoneNumber } = mobileValidation(mobile)
     let accessToken:string;
     let refreshToken :string;
-    await this.categoryService.findById(+category)
-    let doctor = await this.doctorRepository.findOneBy({mobile : phoneNumber})
+    const [doctor, existCheck, categoryExist]= await Promise.all([
+      this.doctorRepository.findOneBy({mobile : phoneNumber}),
+      this.doctorRepository.exists({ where : [
+        { national_code },
+        { Medical_License_number } 
+        ]
+      }),
+      await this.categoryService.findByTitle(category)
+    ])
+    console.log(existCheck);
+    if(existCheck){
+      throw new ConflictException("شما قبلا ثبت نام کرده اید")
+    }
     if(doctor){
       if(doctor.mobile_verify){
         throw new ConflictException("شما ثبت نام خود را تکمیل کرده اید")
@@ -34,7 +45,7 @@ export class DoctorsService {
       refreshToken
       const { Location } = await this.s3Service.uploadFile(image,"Doctors")
       await this.doctorRepository.update({mobile : phoneNumber},{
-         category,
+         category : categoryExist.slug,
          description,
          image : Location,
          Medical_License_number, 
@@ -61,24 +72,21 @@ export class DoctorsService {
   }
 
   async update(medical_license: string, updateDoctorDto: UpdateDoctorDto, image : Express.Multer.File) {
-    console.log("object");
-    const {description,first_name,last_name, mobile, national_code} = updateDoctorDto
-    let destination : string;
-    const updateData : any = {}
-
+    const { mobile } = updateDoctorDto
+    let updateData : any = {}
     await this.findOneByLicense(medical_license)
+    for (const data in updateDoctorDto) {
+      if(updateDoctorDto[data]){
+        updateData[data] = updateDoctorDto[data]
+      }
+    }
     if(image){
       const { Location } = await this.s3Service.uploadFile(image, "Doctors")
-      destination = Location
+      updateData.image = Location
     }
-    if(description) updateData.description = description
-    if(first_name) updateData.first_name = first_name
-    if(last_name) updateData.last_name = last_name
-    if(national_code) updateData.national_code = national_code
-    if(destination) updateData.image = destination
     if(mobile){
       const { phoneNumber } = mobileValidation(mobile)
-      updateData.description = phoneNumber
+      updateData.mobile = phoneNumber
     } 
     await this.doctorRepository.update({Medical_License_number : medical_license},{
       ...updateData
@@ -92,9 +100,11 @@ export class DoctorsService {
     return {message : "doctor deleted successfully"}
   }
   async register(changeStatusDto : ChangeStatusDto){
-    const { Medical_License_number, status } = changeStatusDto
+    const { Medical_License_number, status , description} = changeStatusDto
     let doctor = await this.findOneByLicense(Medical_License_number)
+    const date = new Date()
     doctor.status = status
+    doctor.description = `${description}\n changed at ${date}`
     await this.doctorRepository.save(doctor)
     return {message : `تغیر کرد ${status} وضعیت کاربر به`}
   }
