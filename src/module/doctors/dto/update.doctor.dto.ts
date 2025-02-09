a5959c7ca85812ba51855baf8d5f59744af6e6d6
Pooya-Ebad <pooya7009@gmail.com>
2025-02-10 03:09:47 +0330

    update doctor dto updated

--- src/module/doctors/dto/update.doctor.dto.ts
+++ src/module/doctors/dto/update.doctor.dto.ts
@@ -1,5 +1,6 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsMobilePhone, IsString, Length } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsMobilePhone, IsOptional, IsString, Length } from 'class-validator';
 export class UpdateDoctorDto{
     @ApiPropertyOptional({example : "Name"})
     @IsString()
@@ -8,13 +9,11 @@ export class UpdateDoctorDto{
     @IsString()
     last_name : string
     @ApiPropertyOptional({example : "09100000000"})
+    @IsOptional()
+    @Transform(({ value }) => (value === '' ? null : value))
     @IsMobilePhone("fa-IR", {}, {message : "phone number is incorrect"})
     mobile : string
     @ApiPropertyOptional({format : "binary"})
     image : string
-    @ApiPropertyOptional({example: 1000000000})
-    @IsString()
-    @Length(10,10, {message : "کد ملی اشتباه است (حداقل و حداکثر ۱۰ رقم)"})
-    national_code : string
 }
  
